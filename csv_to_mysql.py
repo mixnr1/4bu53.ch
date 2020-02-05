576130b0a35d153719311519f06ac5fb296482a4
mixnr1 <miks.grinbergs@gmail.com>
2020-02-05 17:00:42 +0200

    save

--- csv_to_mysql.py
+++ csv_to_mysql.py
@@ -19,31 +19,33 @@ def list_in_chunks(l, n):
         yield l[i:i+n]
 
 
-#create table which has Primary Key
-mycursor.execute("DROP TABLE IF EXISTS abuse;")
-mycursor.execute(
-        """
-            CREATE TABLE abuse (
-                abuseid int NOT NULL AUTO_INCREMENT, 
-                dateadded VARCHAR(255), 
-                protocol VARCHAR(255), 
-                domain VARCHAR(255), 
-                url_status VARCHAR(255), 
-                threat VARCHAR(255), 
-                tags VARCHAR(255), 
-                urlhaus_link VARCHAR(255), 
-                reporter VARCHAR(255), 
-                PRIMARY KEY (abuseid)
-            );
-        """
-)
+# create table which has Primary Key and FULLTEXT index
+# mycursor.execute("DROP TABLE IF EXISTS abuse;")
+# mycursor.execute(
+#         """
+#             CREATE TABLE abuse (
+#                 abuseid int NOT NULL AUTO_INCREMENT, 
+#                 dateadded VARCHAR(255), 
+#                 protocol VARCHAR(255), 
+#                 domain VARCHAR(255),
+#                 link LONGTEXT, 
+#                 url_status VARCHAR(255), 
+#                 threat VARCHAR(255), 
+#                 tags VARCHAR(255), 
+#                 urlhaus_link VARCHAR(255), 
+#                 reporter VARCHAR(255), 
+#                 FULLTEXT(dateadded, protocol, domain, link, url_status, threat, tags, urlhaus_link, reporter),
+#                 PRIMARY KEY (abuseid)
+#             );
+#         """
+# )
 #create index wich containts all column except primary key
-mycursor.execute("CREATE UNIQUE INDEX `abuseidx` ON abuse (`dateadded`,`protocol`,`domain`, `url_status`,`threat`,`tags`,`urlhaus_link`,`reporter`);")
+# mycursor.execute("CREATE UNIQUE INDEX `abuseidx` ON abuse (`dateadded`,`protocol`,`domain`,`link`,`url_status`,`threat`,`tags`,`urlhaus_link`,`reporter`);")
 
 #load csv file
 csv_dir=config.abuse_data
-command="https_proxy="+config.proxy_settings+" wget -O "+csv_dir+"abuse.csv 'https://urlhaus.abuse.ch/downloads/csv/'"
-os.system(command)
+# command="https_proxy="+config.proxy_settings+" wget -O "+csv_dir+"abuse.csv 'https://urlhaus.abuse.ch/downloads/csv/'"
+# os.system(command)
 #read data from csv file
 csv_files=[entry for entry in os.listdir(csv_dir) if os.path.isfile(os.path.join(csv_dir, entry))]
 for csv_f in csv_files:
@@ -53,19 +55,20 @@ for csv_f in csv_files:
             csv_reader = csv.reader(csv_file, delimiter=',')           
             val=[]
             for row in csv_reader:
+                # print(row)
                 if row[0].startswith('#'):
                     pass
                 else:
                     dateadded = str(row[1])
                     protocol = str(row[2]).split('://')[0]
                     domain = str(row[2]).split('//')[-1].split('/')[0]
-                    # link = str(row[2]).split('//')[-1].split('/', 1)[-1]
+                    link = str(row[2]).split('//')[-1].split('/', 1)[-1]
                     url_status = str(row[3])
                     threat = str(row[4])
                     tags = str(row[5])
                     urlhaus_link = str(row[6])
                     reporter = str(row[7])
-                    rline = (dateadded, protocol, domain, url_status, threat, tags, urlhaus_link, reporter)
+                    rline = (dateadded, protocol, domain, link, url_status, threat, tags, urlhaus_link, reporter)
                     val.append(rline)
             # for i in list(list_in_chunks(val, 1000)):
             #     mydb = mysql.connector.connect(
@@ -75,20 +78,20 @@ for csv_f in csv_files:
             #         database=config.mydb_database
             #     ) 
             #     mycursor = mydb.cursor()
-            #     sql = "INSERT IGNORE INTO abuse (dateadded, protocol, domain, url_status, threat, tags, urlhaus_link, reporter) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-            #     mycursor.executemany(sql, val)
+            #     sql = "INSERT IGNORE INTO abuse (dateadded, protocol, domain, link, url_status, threat, tags, urlhaus_link, reporter) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            #     mycursor.executemany(sql, i)
             #     mydb.commit()
 
             for i in val:
                 dateadded = i[0]
                 protocol = i[1]
                 domain = i[2]
-                # link = i[3]
-                url_status = i[3]
-                threat = i[4]
-                tags = i[5]
-                urlhaus_link = i[6]
-                reporter = i[7]
+                link = i[3]
+                url_status = i[4]
+                threat = i[5]
+                tags = i[6]
+                urlhaus_link = i[7]
+                reporter = i[8]
                 mydb = mysql.connector.connect(
                     host=config.mydb_host,
                     user=config.mydb_user,
@@ -96,12 +99,27 @@ for csv_f in csv_files:
                     database=config.mydb_database
                 ) 
                 mycursor = mydb.cursor()
-                sql=("SELECT count(abuseid) FROM abuse WHERE dateadded='%s' AND protocol='%s' AND domain='%s' AND url_status='%s' AND threat='%s' AND tags='%s' AND urlhaus_link='%s' AND reporter='%s';")
-                mycursor.execute(sql % (dateadded, protocol, domain, url_status, threat, tags, urlhaus_link, reporter))
+                
+                against_fraze=dateadded+protocol+domain+link+url_status+threat+tags+urlhaus_link+reporter
+                like_fraze=dateadded+protocol+domain+link+url_status+threat+tags+urlhaus_link+reporter
+                query=(
+                    """
+                        SELECT count(abuseid) FROM abuse 
+                        WHERE MATCH (dateadded,protocol,domain,link,url_status,threat,tags,urlhaus_link,reporter) AGAINST (%s)
+                        AND CONCAT(dateadded,protocol,domain,link,url_status,threat,tags,urlhaus_link,reporter) LIKE %s
+                    """
+                )
+                mycursor.execute(query, (against_fraze, like_fraze))
                 skaits=mycursor.fetchone()
+
+                # sql=("SELECT count(abuseid) FROM abuse WHERE dateadded='%s' AND protocol='%s' AND domain='%s' AND link='%s' AND url_status='%s' AND threat='%s' AND tags='%s' AND urlhaus_link='%s' AND reporter='%s';")
+                # mycursor.execute(sql % (dateadded, protocol, domain, link, url_status, threat, tags, urlhaus_link, reporter))
+                # skaits=mycursor.fetchone()
                 if skaits[0] == 0:
-                    sql="INSERT IGNORE INTO abuse (dateadded, protocol, domain, url_status, threat, tags, urlhaus_link, reporter) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"
-                    mycursor.execute(sql % (dateadded, protocol, domain, url_status, threat, tags, urlhaus_link, reporter))
+                    print(i)
+                    # sql="INSERT IGNORE INTO abuse (dateadded, protocol, domain, link, url_status, threat, tags, urlhaus_link, reporter) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"
+                    sql="INSERT INTO abuse (dateadded, protocol, domain, link, url_status, threat, tags, urlhaus_link, reporter) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"
+                    mycursor.execute(sql % (dateadded, protocol, domain, link, url_status, threat, tags, urlhaus_link, reporter))
                     mydb.commit()
 
 
